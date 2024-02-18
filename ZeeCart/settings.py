e01152ef5b74e248c9ee8a0669481a9bb9cf137c
Anurag K <anuragremeshank@gmail.com>
2024-02-18 15:51:18 +0530

    .env edit

--- ZeeCart/settings.py
+++ ZeeCart/settings.py
@@ -136,7 +136,7 @@ DATABASES = {
         'NAME': os.getenv("NAME"),
         'HOST': os.getenv("HOST"),
         'PORT': os.getenv("PORT"),
-        'USER': os.getenv("USER"),
+        'USER': os.getenv("USR"),
         'PASSWORD': os.getenv("PASSWORD"),
     }
 }
